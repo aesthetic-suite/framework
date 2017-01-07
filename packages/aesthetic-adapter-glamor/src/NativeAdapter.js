f88e5dad72f1f1afc054e43cdbe5000f18117bad
Miles Johnson <mileswjohnson@gmail.com>
2017-01-07 15:59:45 -0800

    Updated Glamor.

---
+++ packages/aesthetic-adapter-glamor/src/NativeAdapter.js
@@ -0,0 +1,22 @@
+/**
+ * @copyright   2016, Miles Johnson
+ * @license     https://opensource.org/licenses/MIT
+ * @flow
+ */
+
+import { Adapter } from 'aesthetic';
+import { css } from 'glamor';
+
+import type { StyleDeclarationMap, ClassNameMap } from 'aesthetic';
+
+export default class GlamorAdapter extends Adapter {
+  transform(styleName: string, declarations: StyleDeclarationMap): ClassNameMap {
+    const classNames = {};
+
+    Object.keys(declarations).forEach((setName: string) => {
+      classNames[setName] = `${styleName}-${String(css(declarations[setName]))}`;
+    });
+
+    return classNames;
+  }
+}
