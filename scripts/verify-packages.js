ae573e0a5f0058e6e92a1f3dcf8f6b82dc903eaa
Miles Johnson <mileswjohnson@gmail.com>
2017-03-12 18:23:46 -0700

    Updated verify script.

--- scripts/verify-packages.js
+++ scripts/verify-packages.js
@@ -1,27 +1,31 @@
-/* eslint-disable global-require, import/no-dynamic-require */
+/* eslint-disable global-require, no-underscore-dangle, import/no-dynamic-require */
 
 const path = require('path');
 const glob = require('glob');
 
 const rootPath = process.cwd();
 
+// Some packages make use of this constant
+global.__DEV__ = true;
+
 // Attempt to require each file to see if imports are broken
 glob.sync('./packages/*/').forEach((packagePath) => {
   console.log(`Verifying ${path.basename(packagePath)}`);
 
-  try {
-    glob.sync(`${packagePath}/{*.js,lib/*.js}`).forEach((filePath) => {
-      console.log(`\t${path.basename(filePath)}`);
+  glob.sync(`${packagePath}/{*.js,lib/*.js}`).forEach((filePath) => {
+    console.log(`\t${path.basename(filePath)}`);
 
-      const absPath = path.resolve(__dirname, '..', filePath);
+    const absPath = path.resolve(__dirname, '..', filePath);
 
-      // Change directories to utilize lerna linked modules
+    // Change directories to utilize lerna linked modules
+    try {
       process.chdir(path.dirname(absPath));
       require(absPath);
-      process.chdir(rootPath);
-    });
-  } catch (error) {
-    console.log(`\tERROR: ${error.message}`);
+
+    } catch (error) {
+      console.log(`\tERROR: ${error.message}`);
+    }
+
     process.chdir(rootPath);
-  }
+  });
 });
