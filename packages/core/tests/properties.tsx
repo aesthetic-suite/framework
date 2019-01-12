dcfacc792fc996b01a2cc66e40dc52eccb22b416
Miles Johnson <mileswjohnson@gmail.com>
2019-01-11 18:29:35 -0800

    Add test properties file.

---
+++ packages/core/tests/properties.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable */
+
+import React from 'react';
+import ClassNameAesthetic from '../src/ClassNameAesthetic';
+import { ComponentBlock } from '../src/types';
+
+type Theme = { unit: number };
+
+const aesthetic = new ClassNameAesthetic<Theme>();
+
+type Props = { foo?: string };
+
+function Comp(props: Props) {
+  return <div />;
+}
+
+// Direct object
+const ruleset: ComponentBlock = {
+  display: 'block',
+  invalid: 'property',
+
+  ':hover': {
+    color: 'red',
+  },
+
+  '> invalid': {
+    color: 'blue',
+  },
+};
+
+// Styled component using arrow function
+const CompA = aesthetic.withStyles(() => ({
+  display: 'block',
+  invalid: 'property',
+
+  ':hover': {
+    color: 'red',
+  },
+}))(Comp);
+
+// Styled component using arrow function with body
+const CompB = aesthetic.withStyles(() => {
+  return {
+    display: 'block',
+    invalid: 'property',
+
+    ':hover': {
+      color: 'red',
+    },
+  };
+})(Comp);
+
+// Styled component using regular function
+const CompC = aesthetic.withStyles(function() {
+  return {
+    display: 'block',
+    invalid: 'property',
+
+    ':hover': {
+      color: 'red',
+    },
+  };
+})(Comp);
+
+// Styled component with no styles
+const CompD = aesthetic.withStyles(null)(Comp);
