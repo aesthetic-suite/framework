b4dba4abc0497888be9da22f36a6bc85bb581fdd
Miles Johnson <mileswjohnson@gmail.com>
2019-02-21 20:42:55 -0800

    Update deps and module.

--- packages/core/module.js
+++ packages/core/module.js
@@ -7,15 +7,11 @@
 // Because of this, Webpack and other bundlers attempt to import values that do not exist.
 // To mitigate this issue, we need this module specific index file that manually exports.
 
-const Aesthetic = require('./esm/Aesthetic').default;
-const ClassNameAesthetic = require('./esm/ClassNameAesthetic').default;
-const UnifiedSyntax = require('./esm/UnifiedSyntax').default;
-const Ruleset = require('./esm/Ruleset').default;
-const Sheet = require('./esm/Sheet').default;
+import Aesthetic from './esm/Aesthetic';
+import ClassNameAesthetic from './esm/ClassNameAesthetic';
+import UnifiedSyntax from './esm/UnifiedSyntax';
+import Ruleset from './esm/Ruleset';
+import Sheet from './esm/Sheet';
 
-Aesthetic.ClassNameAesthetic = ClassNameAesthetic;
-Aesthetic.UnifiedSyntax = UnifiedSyntax;
-Aesthetic.Ruleset = Ruleset;
-Aesthetic.Sheet = Sheet;
-
-module.exports = Aesthetic;
+export { ClassNameAesthetic, UnifiedSyntax, Ruleset, Sheet };
+export default Aesthetic;
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './tests/bundle.tsx',
-  mode: 'development',
+  mode: 'production',
   module: {
     rules: [
       {
