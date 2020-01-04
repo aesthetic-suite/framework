9cb610b79ae3d7fd6a7049b75da05fc5b0bf2343
Miles Johnson <mileswjohnson@gmail.com>
2020-01-04 14:56:39 -0800

    Add shorthand and compound properties.

--- packages/sss/src/types.ts
+++ packages/sss/src/types.ts
@@ -2,13 +2,167 @@ import CSS from 'csstype';
 
 type Length = string | number;
 
-// SYNTAX
+// PROPERTIES
 
-export interface Rulesets<T> {
-  [selector: string]: T;
+export interface AnimationProperty {
+  delay?: CSS.StandardProperties['animationDelay'];
+  direction?: CSS.AnimationDirectionProperty;
+  duration?: CSS.StandardProperties['animationDuration'];
+  fillMode?: CSS.AnimationFillModeProperty;
+  iterationCount?: CSS.AnimationIterationCountProperty;
+  name?: CSS.AnimationNameProperty;
+  playState?: CSS.AnimationPlayStateProperty;
+  timingFunction?: CSS.AnimationTimingFunctionProperty;
 }
 
-export type Properties = CSS.StandardProperties<Length>;
+export interface BackgroundProperty {
+  attachment?: CSS.BackgroundAttachmentProperty;
+  clip?: CSS.BackgroundClipProperty;
+  color?: CSS.BackgroundColorProperty;
+  image?: CSS.BackgroundImageProperty;
+  origin?: CSS.BackgroundOriginProperty;
+  position?: CSS.BackgroundPositionProperty<Length>;
+  repeat?: CSS.BackgroundRepeatProperty;
+  size?: CSS.BackgroundSizeProperty<Length>;
+}
+
+export interface BorderProperty {
+  color?: CSS.BorderColorProperty;
+  style?: CSS.BorderStyleProperty;
+  width?: CSS.BorderWidthProperty<Length>;
+}
+
+export interface ColumnRuleProperty {
+  color?: CSS.ColumnRuleColorProperty;
+  style?: CSS.ColumnRuleStyleProperty;
+  width?: CSS.ColumnRuleWidthProperty<Length>;
+}
+
+export interface ColumnsProperty {
+  count?: CSS.ColumnCountProperty;
+  width?: CSS.ColumnWidthProperty<Length>;
+}
+
+export interface FlexProperty {
+  basis?: CSS.FlexBasisProperty<Length>;
+  grow?: CSS.Properties['flexGrow'];
+  shrink?: CSS.Properties['flexShrink'];
+}
+
+export interface FontProperty {
+  family?: CSS.FontFamilyProperty;
+  lineHeight?: CSS.LineHeightProperty<Length>;
+  size?: CSS.FontSizeProperty<Length>;
+  stretch?: CSS.FontStretchProperty;
+  style?: CSS.FontStyleProperty;
+  system?: string;
+  variant?: CSS.FontVariantProperty;
+  weight?: CSS.FontWeightProperty;
+}
+
+export interface ListStyleProperty {
+  image?: CSS.ListStyleImageProperty;
+  position?: CSS.ListStylePositionProperty;
+  type?: CSS.ListStyleTypeProperty;
+}
+
+export interface MarginProperty {
+  bottom?: CSS.MarginBottomProperty<Length>;
+  left?: CSS.MarginLeftProperty<Length>;
+  leftRight?: CSS.MarginLeftProperty<Length> | CSS.MarginRightProperty<Length>;
+  right?: CSS.MarginRightProperty<Length>;
+  top?: CSS.MarginTopProperty<Length>;
+  topBottom?: CSS.MarginTopProperty<Length> | CSS.MarginBottomProperty<Length>;
+}
+
+export interface OffsetProperty {
+  anchor?: CSS.OffsetAnchorProperty<Length>;
+  distance?: CSS.OffsetDistanceProperty<Length>;
+  path?: CSS.OffsetPathProperty;
+  position?: string; // NOT UPSTREAM
+  rotate?: CSS.OffsetRotateProperty;
+}
+
+export interface OutlineProperty {
+  color?: CSS.OutlineColorProperty;
+  style?: CSS.OutlineStyleProperty;
+  width?: CSS.OutlineWidthProperty<Length>;
+}
+
+export interface PaddingProperty {
+  bottom?: CSS.PaddingBottomProperty<Length>;
+  left?: CSS.PaddingLeftProperty<Length>;
+  leftRight?: CSS.PaddingLeftProperty<Length> | CSS.PaddingRightProperty<Length>;
+  right?: CSS.PaddingRightProperty<Length>;
+  top?: CSS.PaddingTopProperty<Length>;
+  topBottom?: CSS.PaddingTopProperty<Length> | CSS.PaddingBottomProperty<Length>;
+}
+
+export interface TextDecorationProperty {
+  color?: CSS.TextDecorationColorProperty;
+  line?: CSS.TextDecorationLineProperty;
+  style?: CSS.TextDecorationStyleProperty;
+  thickness?: CSS.TextDecorationThicknessProperty<Length>;
+}
+
+export interface TransitionProperty {
+  delay?: CSS.StandardProperties['transitionDelay'];
+  duration?: CSS.StandardProperties['transitionDuration'];
+  property?: CSS.TransitionPropertyProperty;
+  timingFunction?: CSS.TransitionTimingFunctionProperty;
+}
+
+export type ExpandProperty<B, T> = B | T | (B | T)[];
+
+// SYNTAX
+
+export type CompoundPropertyTypes = 'animationName' | 'fontFamily';
+
+export type ShorthandPropertyTypes =
+  | 'animation'
+  | 'background'
+  | 'border'
+  | 'borderBottom'
+  | 'borderLeft'
+  | 'borderRight'
+  | 'borderTop'
+  | 'columnRule'
+  | 'columns'
+  | 'flex'
+  | 'font'
+  | 'listStyle'
+  | 'margin'
+  | 'offset'
+  | 'outline'
+  | 'padding'
+  | 'textDecoration'
+  | 'transition';
+
+export interface Properties
+  extends Omit<CSS.StandardProperties<Length>, CompoundPropertyTypes | ShorthandPropertyTypes> {
+  animation?: CSS.AnimationProperty | AnimationProperty;
+  animationName?: ExpandProperty<CSS.AnimationNameProperty, Keyframes>;
+  background?: CSS.BackgroundProperty<Length> | BackgroundProperty;
+  border?: CSS.BorderProperty<Length> | BorderProperty;
+  borderBottom?: CSS.BorderBottomProperty<Length> | BorderProperty;
+  borderLeft?: CSS.BorderLeftProperty<Length> | BorderProperty;
+  borderRight?: CSS.BorderRightProperty<Length> | BorderProperty;
+  borderTop?: CSS.BorderTopProperty<Length> | BorderProperty;
+  columnRule?: CSS.ColumnRuleProperty<Length> | ColumnRuleProperty;
+  columns?: CSS.ColumnsProperty<Length> | ColumnsProperty;
+  flex?: CSS.FlexProperty<Length> | FlexProperty;
+  font?: CSS.FontProperty | FontProperty;
+  fontFamily?: ExpandProperty<CSS.FontFamilyProperty, FontFace>;
+  listStyle?: CSS.ListStyleProperty | ListStyleProperty;
+  margin?: CSS.MarginProperty<Length> | MarginProperty;
+  offset?: CSS.OffsetProperty<Length> | OffsetProperty;
+  outline?: CSS.OutlineProperty<Length> | OutlineProperty;
+  padding?: CSS.PaddingProperty<Length> | PaddingProperty;
+  textDecoration?: CSS.TextDecorationProperty<Length> | TextDecorationProperty;
+  transition?: ExpandProperty<CSS.TransitionProperty, TransitionProperty>;
+}
+
+export type NativeProperties = CSS.StandardProperties<Length>;
 
 export type FallbackProperties = CSS.StandardPropertiesFallback<Length>;
 
@@ -18,10 +172,10 @@ export type Attributes = { [A in CSS.HtmlAttributes]?: DeclarationBlock };
 
 export type DeclarationBlock = Properties & Pseudos & Attributes;
 
-export type FontFace = CSS.FontFace & {
+export interface FontFace extends CSS.FontFace {
   local?: string[];
   srcPaths: string[];
-};
+}
 
 export interface Import {
   path: string;
@@ -71,6 +225,10 @@ export type Page = PageBlock & {
   ':left'?: PageBlock;
   ':right'?: PageBlock;
 };
+
+export interface Rulesets<T> {
+  [selector: string]: T;
+}
 
 // LOCAL STYLE SHEET
 
