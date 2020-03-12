e05f6fa4afe9d6056f364da39a102177be2ff9bc
Miles Johnson <mileswjohnson@gmail.com>
2020-03-11 23:36:55 -0700

    Fix build.

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,12 @@
 
 import fs from 'fs';
 import path from 'path';
-import glob from 'fast-glob';
 import autoExternal from 'rollup-plugin-auto-external';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+
+// Order is imporant!
+const packages = ['utils', 'system', 'style', 'sss', 'core', 'react', 'compiler'];
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 const plugins = [
@@ -19,50 +21,47 @@ const plugins = [
 const external = ['rtl-css-js/core', '@aesthetic/system/lib/testing'];
 const targets = [];
 
-glob
-  .sync('packages/*', { onlyDirectories: true })
-  .reverse()
-  .forEach(pkgPath => {
-    external.push(pkgPath.replace('packages', '@aesthetic'));
+packages.forEach(pkg => {
+  external.push(`@aesthetic/${pkg}`, `@aesthetic/${pkg}/lib/testing`);
 
+  targets.push({
+    external,
+    input: `packages/${pkg}/src/index.ts`,
+    output: [
+      {
+        file: `packages/${pkg}/lib/index.js`,
+        format: 'cjs',
+      },
+      {
+        file: `packages/${pkg}/esm/index.js`,
+        format: 'esm',
+      },
+    ],
+    plugins: [
+      ...plugins,
+      autoExternal({
+        packagePath: path.resolve(`packages/${pkg}/package.json`),
+      }),
+    ],
+  });
+
+  if (fs.existsSync(`packages/${pkg}/src/testing.ts`)) {
     targets.push({
-      external,
-      input: `${pkgPath}/src/index.ts`,
+      external: external.concat('./index'),
+      input: `packages/${pkg}/src/testing.ts`,
       output: [
         {
-          file: `${pkgPath}/lib/index.js`,
+          file: `packages/${pkg}/lib/testing.js`,
           format: 'cjs',
         },
         {
-          file: `${pkgPath}/esm/index.js`,
+          file: `packages/${pkg}/esm/testing.js`,
           format: 'esm',
         },
       ],
-      plugins: [
-        ...plugins,
-        autoExternal({
-          packagePath: path.resolve(`${pkgPath}/package.json`),
-        }),
-      ],
+      plugins,
     });
-
-    if (fs.existsSync(`${pkgPath}/src/testing.ts`)) {
-      targets.push({
-        external: external.concat('./index'),
-        input: `${pkgPath}/src/testing.ts`,
-        output: [
-          {
-            file: `${pkgPath}/lib/testing.js`,
-            format: 'cjs',
-          },
-          {
-            file: `${pkgPath}/esm/testing.js`,
-            format: 'esm',
-          },
-        ],
-        plugins,
-      });
-    }
-  });
+  }
+});
 
 export default targets;
