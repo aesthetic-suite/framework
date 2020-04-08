02952d3757e5e2a2024dfd38cdcfc9e6ab8ba0b7
Miles Johnson <mileswjohnson@gmail.com>
2020-04-08 11:03:08 -0700

    fix: Fix rule buffering never emptying.

--- packages/style/src/BaseStyleSheet.ts
+++ packages/style/src/BaseStyleSheet.ts
@@ -41,21 +41,17 @@ export default abstract class BaseStyleSheet {
   }
 
   protected injectRule = (rule: string, index: number) => {
-    // CSS is locked in developer tools when using `insertRule`,
-    // so avoid using it in development so we have full control.
-    // istanbul ignore next
-    if (process.env.NODE_ENV === 'development' && !isSSR()) {
-      this.sheet.textContent += rule;
-    } else {
-      this.sheet.insertRule(rule, index);
-      this.lastIndex = this.sheet.cssRules.length;
-    }
+    this.sheet.insertRule(rule, index);
+    this.lastIndex = this.sheet.cssRules.length;
   };
 
   protected flushRules = () => {
     this.rafHandle = 0;
+
     this.ruleBuffer.forEach((buffer) => {
       this.injectRule(buffer.rule, buffer.index);
     });
+
+    this.ruleBuffer = [];
   };
 }
