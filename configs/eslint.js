6eb8228afb80f5259bd34bfd2a5c15cb771f6604
Miles Johnson <mileswjohnson@gmail.com>
2020-09-18 20:36:19 -0700

    deps: Update to latest.

--- configs/eslint.js
+++ configs/eslint.js
@@ -1,7 +1,14 @@
+/* eslint-disable sort-keys */
+
 module.exports = {
   rules: {
     'import/export': 'off',
     'node/no-missing-import': 'off',
     'prefer-object-spread': 'off',
+
+    // Broken
+    'no-redeclare': 'off',
+    'no-use-before-define': 'off',
+    'no-shadow': 'off',
   },
 };
