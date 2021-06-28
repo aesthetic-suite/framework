e5f2392f62d84381075a68d277b5931b8635ab86
Miles Johnson <mileswjohnson@gmail.com>
2021-06-27 20:14:25 -0700

    Add test engine.

--- packages/style/src/test.ts
+++ packages/style/src/test.ts
@@ -2,11 +2,16 @@ import {
 	CacheManager,
 	ClassName,
 	CSS,
+	Engine,
 	EngineOptions,
+	RenderOptions,
+	RenderResult,
+	Rule,
 	Sheet,
 	SheetManager,
 	SheetType,
 } from '@aesthetic/types';
+import { hyphenate, objectLoop } from '@aesthetic/utils';
 // Rollup compatibility
 import {
 	createCacheManager,
@@ -33,6 +38,51 @@ export function createTestStyleEngine(
 		sheetManager: createTestSheetManager(),
 		...options,
 	});
+}
+
+export function createTestEngine(): Engine<Rule, ClassName> {
+	const noop = () => {};
+
+	const renderVariable = (name: string) => `variable:${hyphenate(name)}`;
+
+	const renderRule = (rule: Rule, options: RenderOptions = {}): RenderResult<ClassName> => {
+		let result = String(options.debug || 'class');
+
+		objectLoop(rule['@variables'], (value, name) => {
+			result += ` ${renderVariable(name)}`;
+		});
+
+		return {
+			result,
+			variants: Object.keys(rule['@variants'] ?? {}).map((variant) => {
+				const types = variant.split('+').map((v) => v.trim());
+
+				return {
+					result: types.map((v) => `variant:${hyphenate(v)}`).join(' '),
+					types,
+				};
+			}),
+		};
+	};
+
+	const engine: Engine<Rule, ClassName> = {
+		direction: 'ltr',
+		prefersColorScheme: () => false,
+		prefersContrastLevel: () => false,
+		renderDeclaration: (prop) => `property:${hyphenate(prop)}`,
+		renderFontFace: () => 'font-face',
+		renderImport: () => 'import',
+		renderKeyframes: (keyframes, name) => `keyframes:${hyphenate(name ?? 'unknown')}`,
+		renderRule,
+		renderRuleGrouped: renderRule,
+		renderVariable,
+		ruleCount: 0,
+		setDirection: noop,
+		setRootVariables: noop,
+		setTheme: noop,
+	};
+
+	return engine;
 }
 
 export function getRenderedStyles(type: Sheet | SheetType): CSS {
